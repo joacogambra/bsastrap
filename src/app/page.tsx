668b30a464eb1400dcd04bdab74b3728c413c15d
joacogambra <joaquingambra@gmail.com>
2024-07-30 20:46:43 -0300

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,160 @@
 import Image from "next/image";
+import banner from '../../public/paint-bsas.png'
+import buzo from '../../public/buzo.png'
+import reme1 from '../../public/reme1.png'
+import show1 from '../../public/1show.jpg'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="pagina">
+
+      <div className="banner">
+
+        <div className="sidebar">
+          <h1>B$A$TRAP</h1>
+          <span>Log In</span>
         </div>
+
+        <div className="main">
+          <div className="main-span">
+            <span className="fecha-lugar">7 y 8 de Diciembre|</span>
+            <span className="fecha-lugar">Parque de la Ciudad</span>
+          </div>
+
+          <Image
+            alt="abc"
+            src={banner}
+            className="imagen-portada">
+          </Image>
+        </div>
+
+      </div>
+      {/* cierre del banner */}
+
+      <div className="shows">
+        <h4>SHOWS</h4>
+
+        <div className="contain-show">
+          <div className="hover">
+            <Image alt="show duki" src={show1} width={82} className="imagen-show"></Image>
+            <p className="numero-dia">4</p>
+            <div className="dia-mes">
+              <p>Sab</p>
+              <p>Dic</p>
+            </div>
+            <span className="place-lugar">Monumental</span>
+          </div>
+          <button className="buy-button">Buy Tickets</button>          
+        </div>
+
+        <div className="contain-show">
+          <div className="hover">
+            <Image alt="show duki" src={show1} width={82} className="imagen-show"></Image>
+            <p className="numero-dia">4</p>
+            <div className="dia-mes">
+              <p>Sab</p>
+              <p>Dic</p>
+            </div>
+            <span className="place-lugar">Monumental</span>
+          </div>
+          <button className="buy-button">Buy Tickets</button>          
+        </div>
+
+        <div className="contain-show">
+          <div className="hover">
+            <Image alt="show duki" src={show1} width={82} className="imagen-show"></Image>
+            <p className="numero-dia">4</p>
+            <div className="dia-mes">
+              <p>Sab</p>
+              <p>Dic</p>
+            </div>
+            <span className="place-lugar">Monumental</span>
+          </div>
+          <button className="buy-button">Buy Tickets</button>          
+        </div>
+
+        <div className="contain-show">
+          <div className="hover">
+            <Image alt="show duki" src={show1} width={82} className="imagen-show"></Image>
+            <p className="numero-dia">4</p>
+            <div className="dia-mes">
+              <p>Sab</p>
+              <p>Dic</p>
+            </div>
+            <span className="place-lugar">Monumental</span>
+          </div>
+          <button className="buy-button">Buy Tickets</button>          
+        </div>
+
+        <div className="contain-show">
+          <div className="hover">
+            <Image alt="show duki" src={show1} width={82} className="imagen-show"></Image>
+            <p className="numero-dia">4</p>
+            <div className="dia-mes">
+              <p>Sab</p>
+              <p>Dic</p>
+            </div>
+            <span className="place-lugar">Monumental</span>
+          </div>
+          <button className="buy-button">Buy Tickets</button>          
+        </div>
+
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="merch">
+
+        <div className="one">
+          <h2>Merch</h2>
+          <p>Toda la indumentaria del BSAS Trap</p>
+          <button className="get-merch">Get Merch</button>
+        </div>
+
+        <div className="two">
+            <div className="container-merch-color">
+                <Image
+                alt="buzo"
+                src={buzo}></Image>
+            </div>
+            <div className="buzos">
+              <h2>BUZOS</h2>
+            </div>
+        </div>
+
+        <div className="three">
+            <div className="container-merch-color">
+                <Image
+                alt="buzo"
+                src={reme1}></Image>
+            </div>
+            <div className="buzos">
+              <h2>REMERAS</h2>
+            </div>
+            
+        </div>
+
+        <div className="four">
+            <div className="container-merch-color">
+                <Image
+                alt="buzo"
+                src={buzo}></Image>
+            </div>
+        </div>
+      
+
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="pre-footer">
+        <h3>Activa las notificaciones en Twitter e Instagram</h3>
+        <p>Playlist de Spotify</p>
       </div>
-    </main>
+
+      <div className="footer">
+          <p>© 2024 BY JOACO. CREATED ON NextJS.</p>
+          <p>TERMINOS Y CONDICIONES</p>
+          <p>ENVIOS Y DEVOLUCIONES</p>
+          <p>PREGUNTAS FRECUENTES</p>
+      </div>
+
+    </div>
   );
 }
